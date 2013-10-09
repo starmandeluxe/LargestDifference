87db7c3444431fc110d7ce5a691d0fe350468f41
Alex K <starmandeluxe2001@gmail.com>
2013-10-09 01:17:30 -0700

    first commit

---
+++ src/LargestDifference.java
@@ -0,0 +1,38 @@
+
+public class LargestDifference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	 //find the largest difference between A[Q] and A[P] such that Q > P.
+	public static int solution(int[] A)
+	{
+		int N = A.length;
+		if (N < 1)
+		{
+			return 0;
+		}
+		//update the max difference if the subtraction is greater
+		
+		int highest = 0;
+		int maxdiff = 0;
+		
+		for (int i = N-1; i >= 0; --i)
+		{
+			//keep track of the largest value
+			if (A[i] > highest)
+				highest = A[i];
+			
+			//keep subtracting it to each element
+			int temp = highest - A[i];
+			
+			if (temp > maxdiff)
+				maxdiff = temp;
+		}
+		
+		return maxdiff;
+	}
+
+}
